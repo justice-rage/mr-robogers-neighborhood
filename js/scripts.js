ea4d15fe60861192fed71e02a849a6b887368afc
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-10 11:15:02 -0800

    add function that passes roboTranslator test 2

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,15 @@ roboTranslator = function returnZeroToNumber(number)  {
   }
 }
 
+// Step 2
+// COMPLETE: push output 0 to X number into array
+
+let numberRangeArray = [];
+
+roboTranslator = function returnZeroToNumber(number)  {
+  for (let index = 0; index <= number; index++) {
+    numberRangeArray.push(index);
+  }
+}
+
 // user interface logic
