fb6d845a9d2ec6c9fd36495182b80c5956bd67bf
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-10 10:31:06 -0800

    add function that passes test 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 // business logic
-// WORKS
-for (let index = 0; index <= 5; index++) {
-  console.log(index);
+
+const number = 5;
+
+// Step 1
+// COMPLETE: output 0 to X number
+roboTranslator = function returnZeroToNumber(number)  {
+  for (let index = 0; index <= number; index++) {
+    console.log(index);
+  }
 }
+
 // user interface logic
