dc7d7664e73bc878bfb9a3c153196cc1494022ef
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-10 16:19:45 -0800

    business logic testing complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,5 @@
 // business logic
 
-
-// STEP 1 - COMPLETE
-// PUSH OUTPUT 0 TO X NUMBER INTO AN ARRAY
-
-let capturedRange = [];
-
-captureNumberRange = function returnZeroToNumber(number)  {
-  for (let index = 0; index <= number; index++) {
-    capturedRange.push(index);
-  }
-}
-
-// -----------------------------------
-
-// STEP 2 - COMPLETE
-// TURN ARRAY OF NUMBERS INTO AN ARRAY WITH SINGLE STRING
-let capturedRangeConvertedToString = capturedRange.toString();
-
-// -----------------------------------
-
-// POTENTIAL STEP 3 - COMPLETE
-// SPLIT SINGLE STRING BY COMMAS
-let finalNumberToStringTransformation = capturedRangeConvertedToString.split(",")
-
-// STEP 3 - INCOMPLETE
-// Numbers that contain a 1: all digits are replaced (all digits) with "Beep!"
-// .toString and .includes - attempt to include together
-
-// hierarchy testing complete
 roboTranslator = function translator(number)  {
   let translation = []
 
