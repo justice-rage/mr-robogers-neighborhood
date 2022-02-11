dc66da4df0c3797753467ec946880397ea421899
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-10 16:10:48 -0800

    roboTranslator passes test 5

--- js/scripts.js
+++ js/scripts.js
@@ -50,12 +50,12 @@ roboTranslator = function translator(number)  {
   let translation = []
 
   for (let index = 0; index <= number; index++) {
-    if (index.toString().includes("1")) {
-      translation.push("Beep!");
+    if (index.toString().includes("3")) {
+      translation.push("Won't you be my neighbor?");
     } else if (index.toString().includes("2")) {
       translation.push("Boop!");
-    } else if (index.toString().includes("3")) {
-      translation.push("Won't you be my neighbor?");
+    } else if (index.toString().includes("1")) {
+      translation.push("Beep!");
     } else {
       translation.push(index);
     }
