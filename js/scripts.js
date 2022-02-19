82e843698c0884df4d573a2bda58f4432aa716cc
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-18 17:47:26 -0800

    add resubmission requirement - function roboTranslator(number)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 // business logic
 
-roboTranslator = function translator(number)  {
+// roboTranslator = function translator(number)  {
+//   let translation = []
+
+//   for (let index = 0; index <= number; index++) {
+//     if (index.toString().includes("3")) {
+//       translation.push("Won't you be my neighbor?");
+//     } else if (index.toString().includes("2")) {
+//       translation.push("Boop!");
+//     } else if (index.toString().includes("1")) {
+//       translation.push("Beep!");
+//     } else {
+//       translation.push(index);
+//     }
+//   }
+//   return translation
+// }
+
+function roboTranslator(number)  {
   let translation = []
 
   for (let index = 0; index <= number; index++) {
