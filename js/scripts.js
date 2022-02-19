e0aea7521861c633efe80e58de52aca32070f0d3
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-02-18 17:48:35 -0800

    remove commented out code

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,5 @@
 // business logic
 
-// roboTranslator = function translator(number)  {
-//   let translation = []
-
-//   for (let index = 0; index <= number; index++) {
-//     if (index.toString().includes("3")) {
-//       translation.push("Won't you be my neighbor?");
-//     } else if (index.toString().includes("2")) {
-//       translation.push("Boop!");
-//     } else if (index.toString().includes("1")) {
-//       translation.push("Beep!");
-//     } else {
-//       translation.push(index);
-//     }
-//   }
-//   return translation
-// }
-
 function roboTranslator(number)  {
   let translation = []
 
